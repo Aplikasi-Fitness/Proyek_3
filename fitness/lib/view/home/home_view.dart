7a189304d6429360e5c6a64b2e3a347b8679b0c1
FarelNouval <ajahfarel49@gmail.com>
2024-11-24 19:39:23 +0700

    edit home view

--- fitness/lib/view/home/home_view.dart
+++ fitness/lib/view/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:fitness/common/colo_extension.dart';
+import 'package:fitness/common_widget/round_button.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -43,7 +45,7 @@ class _HomeViewState extends State<HomeView> {
                     IconButton(
                         onPressed: () {},
                         icon: Image.asset(
-                          "assets/img/notification_actiive.png",
+                          "assets/img/notification_active.png",
                           width: 25,
                           height: 25,
                           fit: BoxFit.fitHeight,
@@ -54,16 +56,172 @@ class _HomeViewState extends State<HomeView> {
                   height: media.width * 0.05,
                 ),
                 Container(
+                  height: media.width * 0.4,
                   decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: TColor.secondaryG),
-                    borderRadius: BorderRadius.circular(media.width * 0.2)
+                      gradient: LinearGradient(colors: TColor.primaryG),
+                      borderRadius: BorderRadius.circular(media.width * 0.075)),
+                  child: Stack(alignment: Alignment.center, children: [
+                    Image.asset(
+                      "assets/img/bg_dots.png",
+                      height: media.width * 0.4,
+                      width: double.maxFinite,
+                      fit: BoxFit.fitHeight,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 35, horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "BMI (Body Mass Index)",
+                                style: TextStyle(
+                                    color: TColor.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              Text(
+                                "You have a normal weight",
+                                style: TextStyle(
+                                    color: TColor.white.withOpacity(0.7),
+                                    fontSize: 12),
+                              ),
+                              SizedBox(
+                                height: media.width * 0.05,
+                              ),
+                              SizedBox(
+                                  width: 120,
+                                  height: 30,
+                                  child: RoundButton(
+                                      title: "View More",
+                                      type: RoundButtonType.bgSGradient,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      onPressed: () {}))
+                            ],
+                          ),
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child: PieChart(
+                              PieChartData(
+                                pieTouchData: PieTouchData(
+                                  touchCallback:
+                                      (FlTouchEvent event, pieTouchResponse) {},
+                                ),
+                                startDegreeOffset: 250,
+                                borderData: FlBorderData(
+                                  show: false,
+                                ),
+                                sectionsSpace: 1,
+                                centerSpaceRadius: 0,
+                                sections: showingSections(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  decoration: BoxDecoration(
+                    color: TColor.primaryColor2.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                )
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Today Target",
+                        style: TextStyle(
+                            color: TColor.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                          width: 75,
+                          height: 25,
+                          child: RoundButton(
+                              title: "Check",
+                              type: RoundButtonType.bgGradient,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              onPressed: () {}))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                Text(
+                  "Activity Status",
+                  style: TextStyle(
+                      color: TColor.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700),
+                ),
+
+                SizedBox(
+                  height: media.width * 0.02,
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  decoration: BoxDecoration(
+                    color: TColor.primaryColor2.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(
+      2,
+      (i) {
+        var color0 = TColor.secondaryColor1;
+
+        switch (i) {
+          case 0:
+            return PieChartSectionData(
+                color: color0,
+                value: 33,
+                title: '',
+                radius: 50,
+                titlePositionPercentageOffset: 0.55,
+                badgeWidget: const Text(
+                  "20,1",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700),
+                ));
+          case 1:
+            return PieChartSectionData(
+              color: Colors.white,
+              value: 75,
+              title: '',
+              radius: 45,
+              titlePositionPercentageOffset: 0.55,
+            );
+
+          default:
+            throw Error();
+        }
+      },
     );
   }
 }
--- fitness/lib/view/main_tab/main_tab_view.dart
+++ fitness/lib/view/main_tab/main_tab_view.dart
@@ -1,5 +1,6 @@
 import 'package:fitness/common/colo_extension.dart';
 import 'package:fitness/common_widget/tab_button.dart';
+import 'package:fitness/view/home/home_view.dart';
 import 'package:flutter/material.dart';
 
 class MainTabView extends StatefulWidget {
@@ -11,10 +12,13 @@ class MainTabView extends StatefulWidget {
 
 class _MainTabViewState extends State<MainTabView> {
   int selectTab = 0;
+  final PageStorageBucket pageBucket = PageStorageBucket();
+  Widget curentTab = const HomeView();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: TColor.white,
+      body: PageStorage(bucket: pageBucket, child: curentTab),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: SizedBox(
         width: 70,
@@ -44,51 +48,61 @@ class _MainTabViewState extends State<MainTabView> {
         decoration: BoxDecoration(color: TColor.white, boxShadow: const [
           BoxShadow(color: Colors.black26, blurRadius: 1, offset: Offset(0, -1))
         ]),
-        height: kToolbarHeight,
+        height: kToolbarHeight + 10,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            TabButton(
-                icon: "assets/img/home_tab.png",
-                selectIcon: "assets/img/home_tab_select.png",
-                isActive: selectTab == 0,
-                onTap: () {
-                  selectTab = 0;
-                  if (mounted) {
-                    setState(() {});
-                  }
-                }),
-            TabButton(
-                icon: "assets/img/activity_tab.png",
-                selectIcon: "assets/img/activity_tab_select.png",
-                isActive: selectTab == 1,
-                onTap: () {
-                  selectTab = 1;
-                  if (mounted) {
-                    setState(() {});
-                  }
-                }),
-                const SizedBox(width: 20,),
-            TabButton(
-                icon: "assets/img/camera_tab.png",
-                selectIcon: "assets/img/camera_tab_select.png",
-                isActive: selectTab == 2,
-                onTap: () {
-                  selectTab = 2;
-                  if (mounted) {
-                    setState(() {});
-                  }
-                }),
-            TabButton(
-                icon: "assets/img/profile_tab.png",
-                selectIcon: "assets/img/profile_tab_select.png",
-                isActive: selectTab == 3,
-                onTap: () {
-                  selectTab = 3;
-                  if (mounted) {
-                    setState(() {});
-                  }
-                })
+            Flexible(
+              child: TabButton(
+                  icon: "assets/img/home_tab.png",
+                  selectIcon: "assets/img/home_tab_select.png",
+                  isActive: selectTab == 0,
+                  onTap: () {
+                    selectTab = 0;
+                    curentTab = const HomeView();
+                    if (mounted) {
+                      setState(() {});
+                    }
+                  }),
+            ),
+            Flexible(
+              child: TabButton(
+                  icon: "assets/img/activity_tab.png",
+                  selectIcon: "assets/img/activity_tab_select.png",
+                  isActive: selectTab == 1,
+                  onTap: () {
+                    selectTab = 1;
+                    if (mounted) {
+                      setState(() {});
+                    }
+                  }),
+            ),
+                const SizedBox(width: 40,),
+            Flexible(
+              child: TabButton(
+                  icon: "assets/img/camera_tab.png",
+                  selectIcon: "assets/img/camera_tab_select.png",
+                  isActive: selectTab == 2,
+                  onTap: () {
+                    selectTab = 2;
+                    if (mounted) {
+                      setState(() {});
+                    }
+                  }),
+            ),
+            Flexible(
+              child: TabButton(
+                  icon: "assets/img/profile_tab.png",
+                  selectIcon: "assets/img/profile_tab_select.png",
+                  isActive: selectTab == 3,
+                  onTap: () {
+                    selectTab = 3;
+                    if (mounted) {
+                      setState(() {});
+                    }
+                  }),
+            )
           ],
         ),
       )),
